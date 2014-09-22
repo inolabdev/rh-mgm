3d7b20e154228a27b02572dfe70f82c40be9656b
emaposse <eusebiomaposse@outlook.com>
2014-09-22 14:58:20 +0200

    Added document from rh model.

---
+++ rh/src/main/java/mz/inolabdev/model/Document.java
@@ -0,0 +1,83 @@
+package mz.inolabdev.model;
+
+import java.util.Date;
+
+public class Document extends IdEntity {
+
+	private static final long serialVersionUID = -4654818069177689714L;
+	private String name;
+	private DocumentType documentType;
+	private String category;
+	private String status;
+	private Date created_at;	
+	private Date updated_at;
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	private String published_to;
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getPublished_to() {
+		return published_to;
+	}
+
+	public void setPublished_to(String published_to) {
+		this.published_to = published_to;
+	}
+
+	private Employee employee;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Employee getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+
+	public DocumentType getDocumentType() {
+		return documentType;
+	}
+
+	public void setDocumentType(DocumentType documentType) {
+		this.documentType = documentType;
+	}
+
+	public Date getCreated_at() {
+		return created_at;
+	}
+
+	public void setCreated_at(Date created_at) {
+		this.created_at = created_at;
+	}
+
+	public Date getUpdated_at() {
+		return updated_at;
+	}
+
+	public void setUpdated_at(Date updated_at) {
+		this.updated_at = updated_at;
+	}
+
+}
