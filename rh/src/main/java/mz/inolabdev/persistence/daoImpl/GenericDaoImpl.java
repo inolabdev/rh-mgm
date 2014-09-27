5f96fc9a3af2456d074792de5f65c91940f9243a
admilsoncossa <admilsoncossa@gmail.com>
2014-09-27 19:01:17 +0200

    Added GenericDao to generify Data Access Object for all entities in the system based on simplifying the DAO layer.

---
+++ rh/src/main/java/mz/inolabdev/persistence/daoImpl/GenericDaoImpl.java
@@ -0,0 +1,67 @@
+package mz.inolabdev.persistence.daoImpl;
+
+import java.io.Serializable;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import mz.inolabdev.persistence.dao.GenericDao;
+
+public abstract class GenericDaoImpl<T extends Serializable> implements
+		GenericDao<T> {
+
+	private Class<T> type;
+
+	@SuppressWarnings("unchecked")
+	public GenericDaoImpl() {
+		Type t = getClass().getGenericSuperclass();
+		ParameterizedType pt = (ParameterizedType) t;
+		type = (Class<T>) pt.getActualTypeArguments()[0];
+	}
+
+	@PersistenceContext
+	protected EntityManager em;
+
+	@Override
+	public long count() {
+		String entity = type.getSimpleName();
+		final StringBuffer queryString = new StringBuffer(
+				"select count(ent) from " + entity + " ent");
+		final Query query = this.em.createQuery(queryString.toString());
+
+		return (Long) query.getSingleResult();
+	}
+
+	@Override
+	public T create(final T t) {
+		em.persist(t);
+		return t;
+	}
+
+	@Override
+	public void delete(final Object id) {
+		em.remove(em.getReference(type, id));
+	}
+
+	@Override
+	public T find(final Object id) {
+		return em.find(type, id);
+	}
+
+	@Override
+	public T update(final T t) {
+		return em.merge(t);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<T> getAll() {
+		Query query = em.createQuery("from " + type.getName());
+		return query.getResultList();
+	}
+
+}
---
+++ rh/src/test/java/mz/inolabdev/persistence/JUnit4ClassRunner.java
@@ -0,0 +1,24 @@
+package mz.inolabdev.persistence;
+
+import java.io.FileNotFoundException;
+
+import org.junit.runners.model.InitializationError;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.Log4jConfigurer;
+
+
+public class JUnit4ClassRunner extends SpringJUnit4ClassRunner {
+
+  static {
+    try {
+      Log4jConfigurer.initLogging( "classpath:/conf/log4j.properties" );
+    }
+    catch( FileNotFoundException ex ) {
+      System.err.println( "Cannot Initialize log4j" );
+    }
+  }
+
+  public JUnit4ClassRunner( Class<?> clazz ) throws InitializationError {
+    super( clazz );
+  }
+}
