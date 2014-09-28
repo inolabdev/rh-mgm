41ec4b16eb1de67b0223252a74b52e6a35226294
jmulungo <jj.mulungo@gmail.com>
2014-09-28 15:48:23 +0200

    New menu, named more.

--- rh/src/main/java/mz/inolabdev/rh/viewModel/PagesFlowsViewModel.java
+++ rh/src/main/java/mz/inolabdev/rh/viewModel/PagesFlowsViewModel.java
@@ -2,21 +2,42 @@ package mz.inolabdev.rh.viewModel;
 
 import org.zkoss.bind.annotation.Command;
 import org.zkoss.bind.annotation.Init;
+import org.zkoss.bind.annotation.NotifyChange;
 import org.zkoss.zk.ui.select.annotation.VariableResolver;
 
 @VariableResolver(org.zkoss.zkplus.spring.DelegatingVariableResolver.class)
 public class PagesFlowsViewModel {
-	
+
 	private String page;
+
+	private String activeHome;
+	private String activeMore;
 
 	@Init
 	public void init() {
+		setActiveHome("active");
 		setPage("dashboard.zul");
 	}
 
 	@Command
-	public void addLog() {
-		
+	@NotifyChange({ "page", "activeHome" })
+	public void home() {
+		resetMenu();
+		setActiveMore("active");
+		setPage("dashboard.zul");
+	}
+
+	@Command
+	@NotifyChange({ "page", "activeMore" })
+	public void sideBarMore() {
+		resetMenu();
+		setActiveMore("active");
+		setPage("/views/more.zul");
+	}
+
+	private void resetMenu() {
+		setActiveHome("");
+		setActiveMore("");
 	}
 
 	public String getPage() {
@@ -25,6 +46,22 @@ public class PagesFlowsViewModel {
 
 	public void setPage(String page) {
 		this.page = page;
+	}
+
+	public String getActiveHome() {
+		return activeHome;
+	}
+
+	public void setActiveHome(String activeHome) {
+		this.activeHome = activeHome;
+	}
+
+	public String getActiveMore() {
+		return activeMore;
+	}
+
+	public void setActiveMore(String activeMore) {
+		this.activeMore = activeMore;
 	}
 
 }
