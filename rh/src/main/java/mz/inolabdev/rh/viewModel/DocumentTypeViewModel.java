9be4276109abbee0158c26a32f2de71d2c4efb36
jmulungo <jj.mulungo@gmail.com>
2014-09-28 15:49:51 +0200

    Generated document type crud layout.

---
+++ rh/src/main/java/mz/inolabdev/rh/viewModel/DocumentTypeViewModel.java
@@ -0,0 +1,36 @@
+package mz.inolabdev.rh.viewModel;
+
+import org.zkoss.bind.annotation.AfterCompose;
+import org.zkoss.bind.annotation.ContextParam;
+import org.zkoss.bind.annotation.ContextType;
+import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.Init;
+import org.zkoss.bind.annotation.NotifyChange;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.select.Selectors;
+import org.zkoss.zk.ui.select.annotation.VariableResolver;
+import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zul.Include;
+
+@VariableResolver(org.zkoss.zkplus.spring.DelegatingVariableResolver.class)
+public class DocumentTypeViewModel {
+
+	@Wire("#mainInclude")
+	private Include mainInclude;
+	
+	@AfterCompose
+    public void afterCompose(@ContextParam(ContextType.VIEW) Component view){
+        Selectors.wireComponents(view, this, false);
+    }
+
+	@Init
+	public void init() {
+		
+	}
+
+	@Command
+	@NotifyChange("mainInclude")
+	public void documentTypeList() {
+		mainInclude.setSrc("views/documentType/index.zul");
+	}
+}
