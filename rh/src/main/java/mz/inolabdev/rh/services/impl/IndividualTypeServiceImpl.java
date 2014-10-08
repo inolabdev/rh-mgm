908017e4c88d8d5ab2f97dc248f2dfd5fa44fe50
emaposse <eusebiomaposse@outlook.com>
2014-10-08 12:08:14 +0200

    Create, Individual Type Service and Individual Type Service Implement.

---
+++ rh/src/main/java/mz/inolabdev/rh/services/impl/IndividualTypeServiceImpl.java
@@ -0,0 +1,11 @@
+package mz.inolabdev.rh.services.impl;
+
+import org.springframework.stereotype.Service;
+
+import mz.inolabdev.rh.entity.IndividualType;
+import mz.inolabdev.rh.services.IndividualTypeService;
+@Service("individualTypeService")
+public class IndividualTypeServiceImpl extends
+		GenericServiceImpl<IndividualType> implements IndividualTypeService {
+
+}
