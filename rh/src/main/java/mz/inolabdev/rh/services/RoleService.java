01256c577528fea767ccc800f6e5dadff757f042
emaposse <eusebiomaposse@outlook.com>
2014-10-08 12:06:27 +0200

    Create, Role Servcice and Role Service Implement.

---
+++ rh/src/main/java/mz/inolabdev/rh/services/RoleService.java
@@ -0,0 +1,19 @@
+package mz.inolabdev.rh.services;
+
+import java.util.List;
+
+import mz.inolabdev.rh.entity.Role;
+
+public interface RoleService {
+    public Role create(Role role);  
+
+	public List<Role> getAll();
+
+	public Role find(Long id);
+
+	public Role update(Role role);
+
+	public long count();
+
+	public void delete(Object id);
+}
