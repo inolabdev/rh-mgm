58a2fa537c4e2db977bd5d7fd881a8ec9b0597e4
emaposse <eusebiomaposse@outlook.com>
2014-10-08 11:48:30 +0200

    Fix columns names from Document Type class.

--- rh/src/main/java/mz/inolabdev/rh/entity/DocumentType.java
+++ rh/src/main/java/mz/inolabdev/rh/entity/DocumentType.java
@@ -1,11 +1,14 @@
 package mz.inolabdev.rh.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.Table;
-
+@Entity
 @Table(name = "document_types")
 public class DocumentType extends IdEntity {
 
 	private static final long serialVersionUID = 1L;
+	@Column(name = "type")
 	private String type;
 
 	public String getType() {
