292cf4fcbe4085eb130256e6c1ec0294e6d3f970
emaposse <eusebiomaposse@outlook.com>
2014-10-08 11:25:44 +0200

    Create, new Individual Type Class.

---
+++ rh/src/main/java/mz/inolabdev/rh/entity/IndividualType.java
@@ -0,0 +1,35 @@
+package mz.inolabdev.rh.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "individual_types")
+public class IndividualType extends IdEntity {
+
+	private static final long serialVersionUID = 1L;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "description")
+	private String description;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
