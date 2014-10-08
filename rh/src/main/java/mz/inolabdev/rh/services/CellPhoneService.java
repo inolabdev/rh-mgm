ea9594fad16a6d60162e1119bc40565de1ffda9f
emaposse <eusebiomaposse@outlook.com>
2014-10-08 12:01:48 +0200

    Create, new Cell phone Service and Cell Phone Service Implement.

---
+++ rh/src/main/java/mz/inolabdev/rh/services/CellPhoneService.java
@@ -0,0 +1,19 @@
+package mz.inolabdev.rh.services;
+
+import java.util.List;
+
+import mz.inolabdev.rh.entity.Cellphone;
+
+public interface CellPhoneService {
+	public Cellphone create(Cellphone cellphone);
+
+	public List<Cellphone> getAll();
+
+	public Cellphone find(Long id);
+
+	public Cellphone update(Cellphone cellphone);
+
+	public long count();
+
+	public void delete(Object id);
+}
