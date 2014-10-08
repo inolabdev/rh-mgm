28014e1bdd045d00382475fe5e9ca5a214779da3
emaposse <eusebiomaposse@outlook.com>
2014-10-08 11:47:46 +0200

    Fix columns names, from Education Level Class.

--- rh/src/main/java/mz/inolabdev/rh/entity/EducationLevel.java
+++ rh/src/main/java/mz/inolabdev/rh/entity/EducationLevel.java
@@ -1,11 +1,16 @@
 package mz.inolabdev.rh.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Entity
 @Table(name = "education_levels")
 public class EducationLevel extends IdEntity {
 
 	private static final long serialVersionUID = -2376892484957010589L;
+
+	@Column(name = "description")
 	private String description;
 
 	public String getDescription() {
