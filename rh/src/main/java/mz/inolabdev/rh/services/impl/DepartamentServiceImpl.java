7eb20e83e6d862eb11afd746b830686a7f6a6440
emaposse <eusebiomaposse@outlook.com>
2014-10-08 12:02:36 +0200

    Create Departament Service and Departament Service Implement.

---
+++ rh/src/main/java/mz/inolabdev/rh/services/impl/DepartamentServiceImpl.java
@@ -0,0 +1,11 @@
+package mz.inolabdev.rh.services.impl;
+
+import org.springframework.stereotype.Service;
+
+import mz.inolabdev.rh.entity.Department;
+import mz.inolabdev.rh.services.DepartamentService;
+@Service("departamentService")
+public class DepartamentServiceImpl extends GenericServiceImpl<Department>
+		implements DepartamentService {
+
+}
