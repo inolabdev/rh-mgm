33adbec9f4dab02791dc97245445c2fdae0e4905
emaposse <eusebiomaposse@outlook.com>
2014-10-08 12:04:21 +0200

    Create, Education Lavel Service and Education Lavel Service Implement.

---
+++ rh/src/main/java/mz/inolabdev/rh/services/EducationLavelService.java
@@ -0,0 +1,20 @@
+package mz.inolabdev.rh.services;
+
+import java.util.List;
+
+import mz.inolabdev.rh.entity.EducationLevel;
+
+public interface EducationLavelService {
+
+	public EducationLevel create(EducationLevel educationLevel);
+
+	public List<EducationLevel> getAll();
+
+	public EducationLevel find(Long id);
+
+	public EducationLevel update(EducationLevel educationLevel);
+
+	public long count();
+
+	public void delete(Object id);
+}
---
+++ rh/src/main/java/mz/inolabdev/rh/services/impl/EducationLavelImpl.java
@@ -0,0 +1,11 @@
+package mz.inolabdev.rh.services.impl;
+
+import org.springframework.stereotype.Service;
+
+import mz.inolabdev.rh.entity.EducationLevel;
+import mz.inolabdev.rh.services.EducationLavelService;
+@Service("educationLavel")
+public class EducationLavelImpl extends GenericServiceImpl<EducationLevel>
+		implements EducationLavelService {
+
+}
