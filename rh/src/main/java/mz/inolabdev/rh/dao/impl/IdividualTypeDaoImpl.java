1abadc691023344d05416567c9b9ddc2f5d9d419
emaposse <eusebiomaposse@outlook.com>
2014-10-08 12:13:52 +0200

    Create, Individual Type Dao Implement.

---
+++ rh/src/main/java/mz/inolabdev/rh/dao/impl/IdividualTypeDaoImpl.java
@@ -0,0 +1,12 @@
+package mz.inolabdev.rh.dao.impl;
+
+import org.springframework.stereotype.Repository;
+
+import mz.inolabdev.rh.dao.IndividualTypeDao;
+import mz.inolabdev.rh.entity.IndividualType;
+
+@Repository("IdividualType")
+public class IdividualTypeDaoImpl extends GenericDaoImpl<IndividualType> implements
+		IndividualTypeDao {
+
+}
