0aed158222f50530f20e91cf55c579091b8596be
admilsoncossa <admilsoncossa@gmail.com>
2014-10-11 02:11:12 +0200

    Added EqualsUtil to perform equality.

---
+++ rh/src/main/java/mz/inolabdev/rh/entity/EnumUtils.java
@@ -0,0 +1,13 @@
+package mz.inolabdev.rh.entity;
+
+public final class EnumUtils {
+
+	// We can add here our common status if necessary
+	enum DefaultStatus {
+		NEW, CLOSED, PUBLISHED, UN_PUPLISHED, NONE
+	}
+	
+	enum DefaultSubUnits {
+		IT, HR, FINANCE, SALES, ACCOUNTING, MARKETING, ADMINISTRATION
+	}
+}
