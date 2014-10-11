a088011b5c6ef8003a1576c1022930e696e94661
admilsoncossa <admilsoncossa@gmail.com>
2014-10-11 02:00:56 +0200

    Identation.

--- rh/src/main/java/mz/inolabdev/rh/dao/impl/ContactPointDaoImpl.java
+++ rh/src/main/java/mz/inolabdev/rh/dao/impl/ContactPointDaoImpl.java
@@ -1,9 +1,10 @@
 package mz.inolabdev.rh.dao.impl;
 
-import org.springframework.stereotype.Repository;
-
 import mz.inolabdev.rh.dao.ContactPointDao;
 import mz.inolabdev.rh.entity.ContactPoint;
+
+import org.springframework.stereotype.Repository;
+
 @Repository("contactPointDao")
 public class ContactPointDaoImpl extends GenericDaoImpl<ContactPoint> implements
 		ContactPointDao {
