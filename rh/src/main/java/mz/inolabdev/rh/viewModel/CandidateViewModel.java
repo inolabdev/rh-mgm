8d22606a9fd4f584e4acf16e7d7288800c9d4c05
jmulungo <jj.mulungo@gmail.com>
2014-10-15 11:45:42 +0200

    Fixed recruitment index path.

--- rh/src/main/java/mz/inolabdev/rh/viewModel/CandidateViewModel.java
+++ rh/src/main/java/mz/inolabdev/rh/viewModel/CandidateViewModel.java
@@ -3,7 +3,6 @@ package mz.inolabdev.rh.viewModel;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 
 import mz.inolabdev.rh.entity.Candidate;
