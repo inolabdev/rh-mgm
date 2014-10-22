b31ecadcd92f78899f80626aacc7c2c53d175f4f
admilsoncossa <admilsoncossa@gmail.com>
2014-10-22 13:13:07 +0200

    A file belongs to a some one!

--- rh/src/main/java/mz/inolabdev/rh/entity/File.java
+++ rh/src/main/java/mz/inolabdev/rh/entity/File.java
@@ -1,13 +1,35 @@
 package mz.inolabdev.rh.entity;
 
 import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
-import com.mysql.jdbc.Blob;
-
+@Entity
+@Table(name = "files")
+@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name="FROM_CLASS", discriminatorType=DiscriminatorType.STRING)
 public class File extends IdEntity {
 
 	private static final long serialVersionUID = 2889402583854838920L;
+	
+	@ManyToOne
+	@JoinColumn(name = "holder_id")
+	private Individual holder;
+	
+	public Individual getHolder() {
+		return holder;
+	}
+
+	public void setHolder(Individual holder) {
+		this.holder = holder;
+	}
 
 	@Column(name = "file_name")
 	private String fileName;
