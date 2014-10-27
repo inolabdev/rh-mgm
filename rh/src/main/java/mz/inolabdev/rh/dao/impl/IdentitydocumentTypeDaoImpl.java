573e8d1c46909b94c7be1551ca537b1a6316dd08
emaposse <eusebiomaposse@outlook.com>
2014-10-27 16:02:14 +0200

    Create identity document type dao and dao implement.

---
+++ rh/src/main/java/mz/inolabdev/rh/dao/impl/IdentitydocumentTypeDaoImpl.java
@@ -0,0 +1,14 @@
+package mz.inolabdev.rh.dao.impl;
+
+import mz.inolabdev.rh.dao.IdentityDocumentTypeDao;
+import mz.inolabdev.rh.entity.IdentityDocumentType;
+
+import org.springframework.stereotype.Repository;
+
+@Repository("identityDocumentTypeDao")
+public class IdentitydocumentTypeDaoImpl extends
+		GenericDaoImpl<IdentityDocumentType> implements IdentityDocumentTypeDao {
+	{
+
+	}
+}
