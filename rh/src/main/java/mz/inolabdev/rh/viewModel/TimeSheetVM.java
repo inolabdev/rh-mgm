a566c29a1f86aeb94b277bd262cbef72d7f35f9d
jmulungo <jj.mulungo@gmail.com>
2014-11-05 14:55:31 +0200

    Timesheet implement..! Part 1.

--- rh/src/main/java/mz/inolabdev/rh/viewModel/TimeSheetVM.java
+++ rh/src/main/java/mz/inolabdev/rh/viewModel/TimeSheetVM.java
@@ -4,8 +4,10 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 import mz.inolabdev.rh.entity.Activity;
 import mz.inolabdev.rh.entity.Project;
@@ -18,15 +20,26 @@ import org.zkoss.bind.annotation.ContextParam;
 import org.zkoss.bind.annotation.ContextType;
 import org.zkoss.bind.annotation.ExecutionArgParam;
 import org.zkoss.bind.annotation.Init;
+import org.zkoss.util.resource.Labels;
 import org.zkoss.zhtml.Ol;
+import org.zkoss.zhtml.Table;
+import org.zkoss.zhtml.Td;
+import org.zkoss.zhtml.Tr;
 import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.event.EventListener;
+import org.zkoss.zk.ui.event.Events;
 import org.zkoss.zk.ui.select.Selectors;
 import org.zkoss.zk.ui.select.annotation.VariableResolver;
 import org.zkoss.zk.ui.select.annotation.Wire;
 import org.zkoss.zk.ui.select.annotation.WireVariable;
+import org.zkoss.zk.ui.util.Clients;
 import org.zkoss.zul.Div;
+import org.zkoss.zul.Intbox;
 import org.zkoss.zul.ListModelList;
 import org.zkoss.zul.Listbox;
+import org.zkoss.zul.Messagebox;
 
 @VariableResolver(org.zkoss.zkplus.spring.DelegatingVariableResolver.class)
 public class TimeSheetVM extends AbstractViewModel {
@@ -38,6 +51,12 @@ public class TimeSheetVM extends AbstractViewModel {
 
 	@Wire
 	private Listbox lbxActivities;
+
+	@Wire
+	private Listbox lbxTimesheet;
+
+	@Wire
+	private Table tblTimesheet;
 
 	@WireVariable
 	private ProjectService projectService;
@@ -54,10 +73,8 @@ public class TimeSheetVM extends AbstractViewModel {
 		this.target = target;
 		this.ol = ol;
 
-		lbxProjects
-				.setModel(new ListModelList<Project>(projectService.getAll()));
-		
 		week = generateWeek();
+		drawnTimesheetTable();
 	}
 
 	@Init
@@ -74,6 +91,215 @@ public class TimeSheetVM extends AbstractViewModel {
 		lbxActivities.setModel(new ListModelList<Activity>(project
 				.getActivities()));
 
+	}
+
+	@Command
+	public void saveTimisheet() {
+
+		int size = tblTimesheet.getChildren().size();
+		Project pro;
+
+		for (int i = 1; i < size; i++) {
+
+			Tr tr = (Tr) tblTimesheet.getChildren().get(i);
+
+			int trTds = tr.getChildren().size();
+
+			for (int j = 0; j < trTds; j++) {
+
+				Td td = (Td) tr.getChildren().get(j);
+
+				if (j == 0) {
+
+					Listbox selectedProj = (Listbox) td.getChildren().get(0);
+
+					if (selectedProj.getSelectedItem() == null)
+						Clients.showNotification(
+								"Selecione um projecto antes de submeter",
+								"warning", tblTimesheet, "before_center", -1);
+
+					else {
+						pro = selectedProj.getSelectedItem().getValue();
+					}
+
+				} else if (j == 1) {
+
+					Listbox selectedAct = (Listbox) td.getChildren().get(0);
+
+					if (selectedAct.getSelectedItem() == null)
+						Clients.showNotification(
+								"Selecione uma actividade antes de submeter",
+								"warning", tblTimesheet, "before_center", -1);
+
+					else {
+						Activity act = selectedAct.getSelectedItem().getValue();
+					}
+
+				} else if (j > 1) {
+
+					Map<String, Integer> actDayHour = new HashMap<String, Integer>();
+
+					Intbox ibx = (Intbox) td.getChildren().get(0);
+
+					actDayHour.put(week.get(j - 2), ibx.getValue());
+				}
+
+			}
+
+		}
+
+	}
+
+	@Command
+	public void newLine() {
+
+		drawnTimesheetTable();
+	}
+
+	@Command
+	public void removeLine() {
+
+		tblTimesheet.getChildren().clear();
+	}
+
+	@Command
+	public void clearFiels() {
+
+		int size = tblTimesheet.getChildren().size();
+
+		for (int i = 1; i < size; i++) {
+
+			Component cp = null;
+			
+			try {
+				
+				cp = tblTimesheet.getChildren().get(i);
+
+			} catch (IndexOutOfBoundsException iob) {
+				
+				cp = tblTimesheet.getChildren().get(i-1);
+
+			} finally {
+				
+				cp.getChildren().clear();
+			}
+		}
+		
+		drawnTimesheetTable();
+	}
+
+	@Command
+	public void pending() {
+
+		final HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("target", target);
+		map.put("breadcrumb", ol);
+		target.getChildren().clear();
+
+		links = new ArrayList<String>();
+		links.add("Timesheets");
+		links.add("Pendentes");
+		drawnBreadcrumb("fa fa-desktop", "Horas", links);
+
+		Executions.createComponents("views/times/timesheet/pending.zul",
+				target, map);
+	}
+
+	@Command
+	public void aprooved() {
+
+		final HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("target", target);
+		map.put("breadcrumb", ol);
+		target.getChildren().clear();
+
+		links = new ArrayList<String>();
+		links.add("Timesheets");
+		links.add("Aprovados");
+		drawnBreadcrumb("fa fa-desktop", "Horas", links);
+
+		Executions.createComponents("views/times/timesheet/aprooved.zul",
+				target, map);
+	}
+
+	@Command
+	public void reprooved() {
+
+		final HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("target", target);
+		map.put("breadcrumb", ol);
+		target.getChildren().clear();
+
+		links = new ArrayList<String>();
+		links.add("Timesheets");
+		links.add("Reprovados");
+		drawnBreadcrumb("fa fa-desktop", "Horas", links);
+
+		Executions.createComponents("views/times/timesheet/reprooved.zul",
+				target, map);
+	}
+
+	@Command
+	public void current() {
+
+		final HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("target", target);
+		map.put("breadcrumb", ol);
+		target.getChildren().clear();
+
+		links = new ArrayList<String>();
+		links.add("Timesheets");
+		links.add("Actual");
+		drawnBreadcrumb("fa fa-desktop", "Horas", links);
+
+		Executions.createComponents("views/times/timesheet/index.zul", target,
+				map);
+	}
+
+	private void drawnTimesheetTable() {
+
+		Tr tr = new Tr();
+		tr.setParent(tblTimesheet);
+
+		Td tdpro = new Td();
+		tdpro.setParent(tr);
+
+		final Listbox lbxPro = new Listbox();
+		lbxPro.setModel(new ListModelList<Project>(projectService.getAll()));
+		lbxPro.setMold("select");
+		lbxPro.setSclass("form-control inoLabFont chzn-select");
+		lbxPro.setParent(tdpro);
+
+		Td tdact = new Td();
+		tdact.setParent(tr);
+
+		final Listbox lbxAct = new Listbox();
+		lbxAct.setMold("select");
+		lbxAct.setSclass("form-control inoLabFont");
+		lbxAct.setParent(tdact);
+
+		lbxPro.addEventListener(Events.ON_SELECT, new EventListener<Event>() {
+			public void onEvent(Event event) {
+
+				Project pro = lbxPro.getSelectedItem().getValue();
+				lbxAct.setModel(new ListModelList<Activity>(pro.getActivities()));
+			}
+		});
+
+		for (int i = 0; i < 7; i++) {
+
+			Td tdHi = new Td();
+			tdHi.setParent(tr);
+
+			Intbox ibx = new Intbox();
+			ibx.setParent(tdHi);
+			ibx.setSclass("form-control inoLabFont");
+			ibx.setValue(0);
+			ibx.setConstraint("no empty: Não pode ser vazio!");
+			ibx.setMaxlength(1);
+		}
+
+		target.invalidate();
 	}
 
 	private List<String> generateWeek() {
