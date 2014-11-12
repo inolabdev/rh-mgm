bc9f210a0a0f66bd1b5e22c81e922e6b44001f6c
admilsoncossa <admilsoncossa@gmail.com>
2014-11-12 12:45:48 +0200

    Added Text column definition.

--- rh/src/main/java/mz/inolabdev/rh/entity/Comment.java
+++ rh/src/main/java/mz/inolabdev/rh/entity/Comment.java
@@ -1,5 +1,6 @@
 package mz.inolabdev.rh.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -9,6 +10,7 @@ public class Comment extends IdEntity{
 
 	private static final long serialVersionUID = 6058550218632091953L;
 	
+	@Column(columnDefinition = "TEXT")
 	private String text;
 
 	public String getText() {
