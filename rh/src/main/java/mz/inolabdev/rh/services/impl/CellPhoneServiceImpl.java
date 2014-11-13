f020d53767a7f6a76415ceb3aa5fed3cde9b4cfb
emaposse <eusebiomaposse@outlook.com>
2014-11-13 10:50:07 +0200

    Remove Cell Phone Service Implement.

--- rh/src/main/java/mz/inolabdev/rh/services/impl/CellPhoneServiceImpl.java
+++ rh/src/main/java/mz/inolabdev/rh/services/impl/CellPhoneServiceImpl.java
@@ -1,13 +1,11 @@
 package mz.inolabdev.rh.services.impl;
 
+import mz.inolabdev.rh.dao.CellPhoneDao;
+import mz.inolabdev.rh.entity.Cellphone;
+import mz.inolabdev.rh.services.CellPhoneService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import mz.inolabdev.rh.dao.CellPhoneDao;
-import mz.inolabdev.rh.dao.ContactPointDao;
-import mz.inolabdev.rh.entity.Cellphone;
-import mz.inolabdev.rh.entity.ContactPoint;
-import mz.inolabdev.rh.services.CellPhoneService;
 
 @Service("cellPhoneService")
 public class CellPhoneServiceImpl extends GenericServiceImpl<Cellphone>
