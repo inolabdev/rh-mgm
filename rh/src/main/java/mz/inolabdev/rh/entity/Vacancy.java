a2981b148de3ac46085114b2f293a64f404c4b26
admilsoncossa <admilsoncossa@gmail.com>
2014-11-16 14:46:56 +0200

    Fixed many to many relation.

--- rh/src/main/java/mz/inolabdev/rh/entity/Vacancy.java
+++ rh/src/main/java/mz/inolabdev/rh/entity/Vacancy.java
@@ -1,15 +1,22 @@
 package mz.inolabdev.rh.entity;
 
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import mz.inolabdev.rh.util.Consts;
 
 @Entity
 @Table(name = "vacancies")
@@ -18,31 +25,38 @@ public class Vacancy extends IdEntity {
 	private static final long serialVersionUID = -8233857444433947377L;
 
 	private String name;
+
+	@Column(columnDefinition = "TEXT")
 	private String description;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name = "begin_date")
+	private Date beginDate;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name = "expiry_date")
+	private Date expiryDate;
 	
 	private String status;
 
 	@OneToOne
 	@JoinColumn(name = "job_title_id")
 	private JobPosition jobTitle;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "sub_unit_id")
 	private SubUnit subUnit;
 
-	@ManyToMany
-	@JoinTable(name = "job_application", joinColumns = { @JoinColumn(name = "vacancy_id") }, inverseJoinColumns = { @JoinColumn(name = "candidate_id") })
-	private Set<Candidate> candidates = new HashSet<Candidate>();
-
-	@ManyToMany
-	@JoinTable(name = "job_application", joinColumns = { @JoinColumn(name = "vacancy_id") }, inverseJoinColumns = { @JoinColumn(name = "hiring_manager_id") })
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "job_interviewers", joinColumns = { @JoinColumn(name = "vacancy_id", referencedColumnName = "id") }, inverseJoinColumns = { @JoinColumn(name = "hiring_manager_id", referencedColumnName = "id") })
 	private Set<Employee> hiringManagers = new HashSet<Employee>();
 
 	@Override
 	public int hashCode() {
-		int hash = 1;
-		hash = hash * 31 + status.hashCode();
-		hash = hash * 31 + (status == null ? 0 : status.hashCode());
+		int hash = Consts.ONE;
+		// hash = hash * 31 + status.hashCode();
+		hash = hash * Consts.THIRTY_ONE
+				+ (getId() == null ? Consts.ONE : getId().hashCode());
 		return hash;
 	}
 
@@ -63,7 +77,12 @@ public class Vacancy extends IdEntity {
 						otherVacancy.hiringManagers)
 				&& EqualsUtil.areEqual(status, otherVacancy.status);
 	}
-
+	
+	@Override
+	public String toString() {
+		return name;
+	}
+	
 	// getters and setters
 	public JobPosition getJobTitle() {
 		return jobTitle;
@@ -89,14 +108,6 @@ public class Vacancy extends IdEntity {
 		this.name = name;
 	}
 
-	public Set<Candidate> getCandidates() {
-		return candidates;
-	}
-
-	public void setCandidates(Set<Candidate> candidates) {
-		this.candidates = candidates;
-	}
-
 	public Set<Employee> getHiringManagers() {
 		return hiringManagers;
 	}
@@ -119,6 +130,22 @@ public class Vacancy extends IdEntity {
 
 	public void setDescription(String description) {
 		this.description = description;
+	}
+
+	public Date getBeginDate() {
+		return beginDate;
+	}
+
+	public Date getExpiryDate() {
+		return expiryDate;
+	}
+
+	public void setBeginDate(Date beginDate) {
+		this.beginDate = beginDate;
+	}
+
+	public void setExpiryDate(Date expiryDate) {
+		this.expiryDate = expiryDate;
 	}
 
 }
